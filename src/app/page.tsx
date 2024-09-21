d1be7089572a5b6b4da52c360cda061d89a2bb91
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-09-21 12:44:45 +0300

    add files

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Layout from "./Layout";
+import SignUp from "./components/SignUp"
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <SignUp/>
+      </main>
+      {/* <Layout>
+        <></>
+
+      </Layout> */}
+    </div>
+  );
+}
